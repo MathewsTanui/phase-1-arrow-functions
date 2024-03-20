c026a683a5c8485bce04cc95facda59d74db0bb3
MathewsTanui <tanuimathews02@gmail.com>
2024-03-20 11:34:00 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,12 @@
+const num = "10"
+const num1 = "20"
+const num2 = "30"
+
+const divide = function() {
+    return 2000 / 100;
+  }
+  
+  const square = (num) => num * num;
+  
+  const add = (num1, num2) => num1 + num2;
+  
